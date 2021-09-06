e5352d4dd98a0dd5a5e7518171ebbf8ee87d32a8
haqh310 <haqh310@gmail.com>
2021-09-06 09:09:58 +0700

    update

---
+++ airline_tickets_agency_management/src/main/java/com/backend/airline_tickets_agency_management/model/service/flight_ticket/ITicketService.java
@@ -0,0 +1,10 @@
+package com.backend.airline_tickets_agency_management.model.service.flight_ticket;
+
+import com.backend.airline_tickets_agency_management.model.entity.flight_ticket.Ticket;
+import com.backend.airline_tickets_agency_management.model.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ITicketService extends IGeneralService<Ticket> {
+    Page<Ticket> findAllByFilter(String passengerName, String ticketCode, String cityName, String flightDate, Pageable pageable);
+}
---
+++ airline_tickets_agency_management/src/main/java/com/backend/airline_tickets_agency_management/model/service/flight_ticket/TicketService.java
@@ -0,0 +1,44 @@
+package com.backend.airline_tickets_agency_management.model.service.flight_ticket;
+
+import com.backend.airline_tickets_agency_management.model.entity.flight_ticket.Ticket;
+import com.backend.airline_tickets_agency_management.model.repository.flight_ticket.ITicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TicketService implements ITicketService{
+    @Autowired
+    private ITicketRepository ticketRepository;
+
+    @Override
+    public Page<Ticket> findAllWithKeyWord(Pageable pageable, String keyword) {
+        return null;
+    }
+
+    @Override
+    public Optional<Ticket> findById(Long id) {
+        return this.ticketRepository.findById(id);
+    }
+
+    @Override
+    public List<Ticket> findAllNormal() {
+        return null;
+    }
+
+    @Override
+    public void save(Ticket ticket) {
+    }
+
+    @Override
+    public void remove(Long id) {
+        this.ticketRepository.deleteTicket(id);
+    }
+
+    @Override
+    public Page<Ticket> findAllByFilter(String passengerName, String ticketCode, String cityName, String flightDate, Pageable pageable) {
+        return this.ticketRepository.findAllByFilter(passengerName, ticketCode, cityName, flightDate, pageable);
+    }
+}
