8e21a9d3ab8d93c8c035359a49f08a111e29c20b
onlinevn113 <onlinevn@gmail.com>
2021-09-13 14:37:13 +0700

    update 13/9

---
+++ airline_tickets_agency_management/src/main/java/com/backend/airline_tickets_agency_management/model/repository/user/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.backend.airline_tickets_agency_management.model.repository.user;
+
+import com.backend.airline_tickets_agency_management.model.entity.user.ERole;
+import com.backend.airline_tickets_agency_management.model.entity.user.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Object> findByName(ERole roleUser);
+}
