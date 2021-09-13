b5f94b4971af57d240f7bda170a18840b2fae33a
Minh Đức Đoàn <you@example.com>
2021-09-13 17:33:03 +0700

    Update merge Trang

---
+++ airline_tickets_agency_management/src/main/java/com/backend/airline_tickets_agency_management/WebSecurityConfig/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.backend.airline_tickets_agency_management.WebSecurityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        // Các trang không yêu cầu login
+        http.authorizeRequests()
+                .antMatchers("api/customer/**").permitAll();
+
+
+    }
+}
